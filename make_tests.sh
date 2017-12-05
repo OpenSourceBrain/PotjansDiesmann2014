00b4927d23d69006cf2538e123daffe550603abb
Padraig Gleeson <p.gleeson@gmail.com>
2017-12-05 19:10:56 +0000

    Even shorter test...

--- make_tests.sh
+++ make_tests.sh
@@ -1,5 +1,5 @@
 # Smaller scale PyNN net to test
-sed -i -e "s/1., # N_scaling/0.1, # N_scaling/g" PyNN/network_params.py
+sed -i -e "s/1., # N_scaling/0.03, # N_scaling/g" PyNN/network_params.py
 
 # Exit before plotting in PyNEST
 sed -i -e 's/# Plot a raster/exit() # Plot a raster/g'  PyNEST/example.py 

8e81054c0b0aede2b2e117941fae5f748a8223f5
Padraig Gleeson <p.gleeson@gmail.com>
2017-12-06 14:48:50 +0000

    Make folder for tests

--- make_tests.sh
+++ make_tests.sh
@@ -1,5 +1,6 @@
 # Smaller scale PyNN net to test
 sed -i -e "s/1., # N_scaling/0.03, # N_scaling/g" PyNN/network_params.py
+mkdir -p PyNN/results
 
 # Exit before plotting in PyNEST
 sed -i -e 's/# Plot a raster/exit() # Plot a raster/g'  PyNEST/example.py 

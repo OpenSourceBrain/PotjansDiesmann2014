d6ddb2d0d45277aa586b3423202128cc32f6abec
Padraig  Gleeson <p.gleeson@gmail.com>
2020-09-03 15:08:03 +0100

    Test on py3

--- make_tests.sh
+++ make_tests.sh
@@ -4,4 +4,4 @@ sed -i -e "s/K_scaling' : 0.5,/K_scaling' : 0.03,/g" PyNN/network_params.py
 mkdir -p PyNN/results
 
 # Exit before plotting in PyNEST
-sed -i -e 's/# Plot a raster/exit() # Plot a raster/g'  PyNEST/example.py 
+###sed -i -e 's/# Plot a raster/exit() # Plot a raster/g'  PyNEST/example.py 

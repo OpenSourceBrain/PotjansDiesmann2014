ff0fb3929b630b033e40c0d46f92fd8998938d21
Padraig Gleeson <p.gleeson@gmail.com>
2021-03-03 18:24:36 +0000

    Fix py3 issue

--- PyNN/microcircuit.py
+++ PyNN/microcircuit.py
@@ -118,7 +118,7 @@ if create_raster_plot and sim.rank() == 0 :
                 n_rec[i][j] = round(N_full[layer][pop] * N_scaling * frac_record_spikes)
             else:
                 n_rec[i][j] = n_record
-    if n_rec > 0:
+    if sum(sum(n_rec,[])) > 0:
         plotting.show_raster_bars(raster_t_min, raster_t_max, n_rec, frac_to_plot,
                               system_params['output_path'] + '/', N_scaling, K_scaling)
 

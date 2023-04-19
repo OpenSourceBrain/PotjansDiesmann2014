6d9212e4a993bf796fc6c5feb2dcd81191c229f3
pgleeson <p.gleeson@gmail.com>
2023-04-19 17:33:29 +0100

    Remove change to example.py

--- make_tests.sh
+++ make_tests.sh
@@ -3,5 +3,3 @@ sed -i -e "s/1., # N_scaling/0.03, # N_scaling/g" PyNN/network_params.py
 sed -i -e "s/K_scaling' : 0.5,/K_scaling' : 0.03,/g" PyNN/network_params.py
 mkdir -p PyNN/results
 
-# Exit before plotting in PyNEST
-sed -i -e 's/# Plot a raster/exit() # Plot a raster/g'  PyNEST/example.py 

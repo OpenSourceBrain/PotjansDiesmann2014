b06c09144470af46bdddfc3a4526b3917676a1ff
pgleeson <p.gleeson@gmail.com>
2023-04-20 17:48:17 +0100

    Update pynn tests for N_scaling=0.1 and K_scaling=0.1

--- make_tests.sh
+++ make_tests.sh
@@ -1,5 +1,5 @@
 # Create a smaller scale PyNN network to test...
-sed -i -e "s/1., # N_scaling/0.03, # N_scaling/g" PyNN/network_params.py
-sed -i -e "s/K_scaling' : 0.5,/K_scaling' : 0.03,/g" PyNN/network_params.py
+sed -i -e "s/1., # N_scaling/0.1, # N_scaling/g" PyNN/network_params.py
+sed -i -e "s/K_scaling' : 0.5,/K_scaling' : 0.1,/g" PyNN/network_params.py
 mkdir -p PyNN/results
 
